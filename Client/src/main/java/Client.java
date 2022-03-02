892c321bb342d72a9427e35ea35d74bdd8de0249
savilin <valkonskyspb@icloud.com>
2022-03-03 00:38:18 +0300

    lesson 6 chatty final changes

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -10,6 +10,48 @@ public class Client {
     private static final String ADDRESS = "localhost";
 
     public static void main(String[] args) {
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+        try{
+            socket = new Socket(ADDRESS,PORT);
+            System.out.println("Connected to server:" + socket.getRemoteSocketAddress());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+
+            Thread thread = new Thread(()->{
+                while(true) {
+                    try {
+                        out.writeUTF(scanner.nextLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.setDaemon(true);
+            thread.start();
+
+            while(true){
+                String str = in.readUTF();
+                if(str.equals("/end")){
+                    System.out.println("lost connection to the server...");
+                    out.writeUTF("/end");
+                    break;
+
+                }else{
+                    System.out.println("Server : " + str);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -6,6 +6,49 @@ import java.util.Scanner;
 public class Server {
 
     public static void main(String[] args) {
-        
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+        try(ServerSocket serverSocket = new ServerSocket(8189)){
+            System.out.println("server running...");
+            socket = serverSocket.accept();
+            System.out.println("Client connected");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            Thread thread = new Thread(()->{
+                while (true){
+                    try {
+                        out.writeUTF(scanner.nextLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            });
+
+            thread.setDaemon(true);
+            thread.start();
+
+            while(true){
+                String str = in.readUTF();
+                if (str.equals("/end")){
+                    System.out.println("client disconnected");
+                    out.writeUTF("/end");
+                    break;
+                }else{
+                    System.out.println("Client: " +str);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
