2a964a57fbe1494bd2a1b53312aeb6a7acf53a3b
savilin <valkonskyspb@icloud.com>
2022-03-06 03:11:42 +0300

    fix javafx problem from view

--- Client/src/main/java/Controller.java
+++ Client/src/main/java/Controller.java
@@ -11,11 +11,6 @@ public class Controller {
     @FXML
     public TextArea textArea;
 
-    public void btn1click(ActionEvent actionEvent) {
-
-        System.out.println("Hello world!");
-    }
-
     public void sendMessage(ActionEvent actionEvent) {
         textArea.appendText(textField.getText()+ "\n");
         textField.clear();
--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Chatty");
+        primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
