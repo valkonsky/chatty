89b2d3814ec3c788dc1611478a79a59f716a1e4c
savilin <valkonskyspb@icloud.com>
2022-03-28 14:30:15 +0300

    add SqliteAuthService.java and realize interaction to database

--- Server/src/main/java/ClientHandler.java
+++ Server/src/main/java/ClientHandler.java
@@ -2,7 +2,6 @@ import constants.Command;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -14,7 +14,8 @@ public class Server {
 
     public Server() {
         clients = new CopyOnWriteArrayList<>();
-        authService = new SimpleAuthService();
+        //authService = new SimpleAuthService();
+        authService = new SqliteAuthService();
         try {
             server = new ServerSocket(PORT);
             System.out.println("server started");
